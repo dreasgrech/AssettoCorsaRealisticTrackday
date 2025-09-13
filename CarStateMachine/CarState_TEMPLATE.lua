176e0fc9b9e39add570f0aa2b288846b38a1d9ce
Andreas Grech <agnt666@gmail.com>
2025-09-13 18:15:48 +0200

    updating state machine state template

--- CarStateMachine/CarState_TEMPLATE.lua
+++ CarStateMachine/CarState_TEMPLATE.lua
@@ -9,22 +9,22 @@ CarStateMachine.CarStateTypeStrings[STATE] = "ThisState"
 CarStateMachine.states_minimumTimeInState[STATE] = 0
 
 -- ENTRY FUNCTION
-CarStateMachine.states_entryFunctions[STATE] = function (carIndex, dt, car, playerCar, storage)
+CarStateMachine.states_entryFunctions[STATE] = function (carIndex, dt, car, carBehind, storage)
 
 end
 
 -- UPDATE FUNCTION
-CarStateMachine.states_updateFunctions[STATE] = function (carIndex, dt, car, playerCar, storage)
+CarStateMachine.states_updateFunctions[STATE] = function (carIndex, dt, car, carBehind, storage)
 
 end
 
 -- TRANSITION FUNCTION
-CarStateMachine.states_transitionFunctions[STATE] = function (carIndex, dt, car, playerCar, storage)
+CarStateMachine.states_transitionFunctions[STATE] = function (carIndex, dt, car, carBehind, storage)
 
 end
 
 -- EXIT FUNCTION
-CarStateMachine.states_exitFunctions[STATE] = function (carIndex, dt, car, playerCar, storage)
+CarStateMachine.states_exitFunctions[STATE] = function (carIndex, dt, car, carBehind, storage)
 
 end
 --]=====]
