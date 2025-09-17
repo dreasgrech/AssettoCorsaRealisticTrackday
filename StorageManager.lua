12ce1171e4307ce3d8385694f18a39b4194ec2a6
Andreas Grech <agnt666@gmail.com>
2025-09-17 21:40:33 +0200

    Adding a docs @class definition for the object returned by the storage manager

--- StorageManager.lua
+++ StorageManager.lua
@@ -1,9 +1,28 @@
 local StorageManager = {}
 
+---@class StorageTable
+---@field enabled boolean
+---@field debugDraw boolean
+---@field drawCarList boolean
+---@field handleSideChecking boolean
+---@field yieldSide integer            -- RaceTrackManager.TrackSide
+---@field overrideAiAwareness boolean
+---@field handleAccidents boolean
+---@field handleOvertaking boolean
+---@field detectCarBehind_meters number
+---@field minPlayerSpeed_kmh number
+---@field yieldMaxOffset_normalized number
+---@field rampSpeed_mps number
+---@field rampRelease_mps number
+---@field overtakeRampSpeed_mps number
+---@field overtakeRampRelease_mps number
+---@field clearAhead_meters number
+---@field minAISpeed_kmh number
+
+---@type StorageTable
 local storageTable = {
     enabled = true,
     debugDraw = false,
-    -- drawOnTop = false,
     drawCarList = true,
     handleSideChecking = true,
     yieldSide = RaceTrackManager.TrackSide.RIGHT,
@@ -20,17 +39,12 @@ local storageTable = {
     overtakeRampRelease_mps = 0.4,
     clearAhead_meters = 6.0,
     minAISpeed_kmh = 35.0,
-    -- distanceToFrontCarToOvertake = 5.0,
-    -- blockSideLateral_meters = 2.2,
-    -- blockSideLongitudinal_meters = 5.5,
-    -- blockSlowdownKmh = 12.0,
-    -- blockThrottleLimit = 0.92,
 }
 
 local storage = ac.storage(storageTable)
 
 ---comment
----@return storageTable
+---@return StorageTable storage
 function StorageManager.getStorage()
     return storage
 end
