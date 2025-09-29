283c99675cdd9af73a6889d8b67b5ff710fbdf67
Andreas Grech <agnt666@gmail.com>
2025-09-29 12:35:40 +0200

    removed the resolved collection from the new completelableindexcollectionmanager

--- DataStructures/CompletableIndexCollectionManager.lua
+++ DataStructures/CompletableIndexCollectionManager.lua
@@ -2,7 +2,6 @@ local CompletableIndexCollectionManager = {}
 
 local collections_lastIndexCreated = {}
 local collections_firstNonResolvedIndex = {}
-local collections_resolved = {}
 
 local lastCreatedCollectionIndex = 0
 
@@ -11,7 +10,6 @@ CompletableIndexCollectionManager.createNewIndex = function()
 
     collections_lastIndexCreated[lastCreatedCollectionIndex] = 0
     collections_firstNonResolvedIndex[lastCreatedCollectionIndex] = 1
-    collections_resolved[lastCreatedCollectionIndex] = false
 
     return lastCreatedCollectionIndex
 end
@@ -24,15 +22,12 @@ CompletableIndexCollectionManager.getFirstNonResolvedIndex = function(collection
     return collections_firstNonResolvedIndex[collectionIndex]
 end
 
--- CompletableIndexCollectionManager.updateFirstNonResolvedIndex = function(collectionIndex)
-CompletableIndexCollectionManager.setAsResolved = function(collectionIndex)
-    collections_resolved[collectionIndex] = true
-
-    -- check the existing collections and update the first non-resolved collection index so that loops iterating over the indexes can skip resolved ones
+CompletableIndexCollectionManager.updateFirstNonResolvedIndex = function(collectionIndex, resolvedCollection)
+    -- check the resolvedCollection and update the first non-resolved collection index so that loops iterating over the indexes can skip resolved ones
     local firstNonResolvedIndex = collections_firstNonResolvedIndex[collectionIndex]
     local lastIndexCreated = collections_lastIndexCreated[collectionIndex]
     for i = firstNonResolvedIndex, lastIndexCreated do
-        if collections_resolved[i] == false then
+        if resolvedCollection[i] == false then
             Logger.log(string.format("[CompletableIndexCollectionManager] #%d Setting first non-resolved index to %d", collectionIndex, i))
             collections_firstNonResolvedIndex[collectionIndex] = i
             break
