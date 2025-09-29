c17b18b3ca329da0dd2e2ba0c1f5c2cce8b165b4
Andreas Grech <agnt666@gmail.com>
2025-09-29 13:08:07 +0200

    started work on creating yellow flag zones

--- RaceTrackManager.lua
+++ RaceTrackManager.lua
@@ -1,5 +1,12 @@
 local RaceTrackManager = {}
 
+local yellowZonesCompletableIndex = CompletableIndexCollectionManager.createNewIndex()
+
+local yellowZones_startSplinePosition = {}
+local yellowZones_endSplinePosition = {}
+local yellowZones_resolved = {}
+
+
 ---@type RaceTrackManager.TrackSide
 ---@enum integer
 RaceTrackManager.TrackSide = {
@@ -11,6 +18,13 @@ RaceTrackManager.TrackSideStrings = {
     [RaceTrackManager.TrackSide.LEFT] = "Left",
     [RaceTrackManager.TrackSide.RIGHT] = "Right",
 }
+
+local sim = ac.getSim()
+local trackLength_meters = sim.trackLengthM
+
+RaceTrackManager.getTrackLengthMeters = function()
+    return trackLength_meters
+end
 
 --- Returns RIGHT if given LEFT and vice versa
 ---@param side RaceTrackManager.TrackSide|integer
@@ -32,6 +46,35 @@ end
 RaceTrackManager.getOvertakingSide = function()
     local yieldSide = RaceTrackManager.getYieldingSide()
     return RaceTrackManager.getOppositeSide(yieldSide)
+end
+
+RaceTrackManager.declareYellowFlagZone = function(yellowZoneEndSplinePosition)
+    local yellowFlagZoneIndex = CompletableIndexCollectionManager.incrementLastIndexCreated(yellowZonesCompletableIndex)
+
+    local storage = StorageManager.getStorage()
+    local yellowZoneSizeMeters = storage.distanceFromAccidentToSeeYellowFlag_meters
+    local yellowZoneSizeNormalized = yellowZoneSizeMeters / trackLength_meters
+
+    local yellowZoneStartSplinePosition = yellowZoneEndSplinePosition - yellowZoneSizeNormalized
+    if yellowZoneStartSplinePosition < 0 then
+        yellowZoneStartSplinePosition = yellowZoneStartSplinePosition + 1.0
+    end
+
+    yellowZones_startSplinePosition[yellowFlagZoneIndex] = yellowZoneStartSplinePosition
+    yellowZones_endSplinePosition[yellowFlagZoneIndex] = yellowZoneEndSplinePosition
+    yellowZones_resolved[yellowFlagZoneIndex] = false
+
+    Logger.log(string.format("[RaceTrackManager] Declared yellow flag zone #%d: start %.3f end %.3f", yellowFlagZoneIndex, yellowZoneStartSplinePosition, yellowZoneEndSplinePosition))
+
+    return yellowFlagZoneIndex
+end
+
+RaceTrackManager.removeYellowFlagZone = function(yellowFlagZoneIndex)
+    yellowZones_startSplinePosition[yellowFlagZoneIndex] = nil
+    yellowZones_endSplinePosition[yellowFlagZoneIndex] = nil
+
+    yellowZones_resolved[yellowFlagZoneIndex] = true
+    CompletableIndexCollectionManager.updateFirstNonResolvedIndex(yellowZonesCompletableIndex, yellowZones_resolved)
 end
 
 return RaceTrackManager
