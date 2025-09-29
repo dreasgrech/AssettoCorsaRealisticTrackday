53679cce92b10a1fe8031369bd0f929c7fbc0cad
Andreas Grech <agnt666@gmail.com>
2025-09-29 16:38:03 +0200

    work on the navigating state

--- CarStateMachine/States/CarState_NavigatingAroundAccident.lua
+++ CarStateMachine/States/CarState_NavigatingAroundAccident.lua
@@ -19,19 +19,15 @@ CarStateMachine.states_updateFunctions[STATE] = function (carIndex, dt, sortedCa
         return
     end
 
-    -- todo: we need to start saving which specific car each car is currently navigating around
-    -- todo: we need to start saving which specific car each car is currently navigating around
-    -- todo: we need to start saving which specific car each car is currently navigating around
-    -- todo: we need to start saving which specific car each car is currently navigating around
-
-    CarOperations.setAICaution(carIndex, 6)
+    --CarOperations.setAICaution(carIndex, 6)
     CarOperations.setAITopSpeed(carIndex, 50)
     CarOperations.setPedalPosition(carIndex, CarOperations.CarPedals.Accelerate, 0.2)
     --CarOperations.setPedalPosition(carIndex, CarOperations.CarPedals.Brake, 0.2)
 
     local car = sortedCarsList[sortedCarsListIndex]
-    -- check if we are close enough to a different accident
-    local closestAccidentIndex, closestAccidentClosestCarIndex = AccidentManager.isCarComingUpToAccident(car, storage.distanceFromAccidentToSeeYellowFlag_meters)
+
+    -- fetch the closest accident to us, in case there's a closer one than the one we're currently navigating around
+    local closestAccidentIndex, closestAccidentClosestCarIndex = AccidentManager.isCarComingUpToAccident(car, storage.distanceToStartNavigatingAroundCarInAccident_meters)
     if not closestAccidentIndex then
         AccidentManager.setCarNavigatingAroundAccident(carIndex, nil, nil)
         return
@@ -43,7 +39,6 @@ CarStateMachine.states_updateFunctions[STATE] = function (carIndex, dt, sortedCa
         -- -- we are closer to a different accident, so switch to navigating around that one instead
         -- CarManager.cars_navigatingAroundAccidentIndex[carIndex] = closestAccidentIndex
     AccidentManager.setCarNavigatingAroundAccident(carIndex, closestAccidentIndex, closestAccidentClosestCarIndex)
-
     accidentIndex = closestAccidentIndex
 
     -- end
@@ -54,7 +49,6 @@ CarStateMachine.states_updateFunctions[STATE] = function (carIndex, dt, sortedCa
     -- local victimCar = ac.getCar(victimCarIndex)
 
     -- determine which car is closest to us by comparing spline positions 
-    local car = sortedCarsList[sortedCarsListIndex]
     -- local culpritCarSplineDistance = math.abs(car.splinePosition - culpritCar.splinePosition)
     -- local victimCarSplineDistance = math.abs(car.splinePosition - victimCar.splinePosition)
 
@@ -75,11 +69,11 @@ CarStateMachine.states_updateFunctions[STATE] = function (carIndex, dt, sortedCa
         return
     end
 
-    local carToNavigateAroundSplineDistance = math.abs(car.splinePosition - closestAccidentClosestCar.splinePosition)
-    if carToNavigateAroundSplineDistance > 0.01 then
-        -- we're more than 1 centimeter away from the car to navigate around, so just drive normally for now
-        return
-    end
+    -- local carToNavigateAroundSplineDistance = math.abs(car.splinePosition - closestAccidentClosestCar.splinePosition)
+    -- if carToNavigateAroundSplineDistance > 0.01 then
+        -- -- we're more than 1 centimeter away from the car to navigate around, so just drive normally for now
+        -- return
+    -- end
 
     local targetOffset = 0
 
@@ -90,7 +84,7 @@ CarStateMachine.states_updateFunctions[STATE] = function (carIndex, dt, sortedCa
     -- local distanceToAccident = car.position:distance(accidentWorldPosition)
 
     local carToNavigateAroundLateralOffset = CarManager.getActualTrackLateralOffset(carToNavigateAround.position)
-    local signOfLateralOffset = carToNavigateAroundLateralOffset < 0 and -1 or 1
+    -- local signOfLateralOffset = carToNavigateAroundLateralOffset < 0 and -1 or 1
     -- now we need to calculate our own offset to use to go around the car to navigating around
     if (math.abs(carToNavigateAroundLateralOffset) < 0.1) then
         -- the car to navigate around is in the middle of the track, so we can pick a side to go around it
@@ -118,21 +112,26 @@ CarStateMachine.states_transitionFunctions[STATE] = function (carIndex, dt, sort
 
     local car = sortedCarsList[sortedCarsListIndex]
 
-    --[====[
-    -- TODO: HAD TO COMMENT CLOSER ACCIDENT CHECK WHILE HANDLING YELLOW FLAG ZONES.  REIMPLEMENT!!!!
-    -- TODO: HAD TO COMMENT CLOSER ACCIDENT CHECK WHILE HANDLING YELLOW FLAG ZONES.  REIMPLEMENT!!!!
-    -- TODO: HAD TO COMMENT CLOSER ACCIDENT CHECK WHILE HANDLING YELLOW FLAG ZONES.  REIMPLEMENT!!!!
-    -- TODO: HAD TO COMMENT CLOSER ACCIDENT CHECK WHILE HANDLING YELLOW FLAG ZONES.  REIMPLEMENT!!!!
     -- check if there's an even closer accident that we should be navigating around instead
-    local newStateDueToAccident = CarStateMachine.handleYellowFlagZone(carIndex, car)
-    if newStateDueToAccident then
-        if CarManager.cars_navigatingAroundAccidentIndex[carIndex] ~= accidentIndex then
-            Logger.log(string.format("[CarState_NavigatingAroundAccident] Car %d switching to navigate around new accident #%d instead of previous accident #%d", carIndex, CarManager.cars_navigatingAroundAccidentIndex[carIndex], accidentIndex))
+    local distanceToStartNavigatingAroundCarInAccident_meters = storage.distanceToStartNavigatingAroundCarInAccident_meters
+    local upcomingAccidentIndex, upcomingAccidentClosestCarIndex = AccidentManager.isCarComingUpToAccident(car, distanceToStartNavigatingAroundCarInAccident_meters)
+    if upcomingAccidentIndex then
+        if upcomingAccidentIndex ~= accidentIndex then
+            Logger.log(string.format("[CarState_NavigatingAroundAccident] Car %d switching to navigate around new accident #%d instead of previous accident #%d", carIndex, upcomingAccidentIndex, accidentIndex))
+            AccidentManager.setCarNavigatingAroundAccident(carIndex, upcomingAccidentIndex, upcomingAccidentClosestCarIndex)
             CarStateMachine.setStateExitReason(carIndex, StateExitReason.FoundCloserAccidentToNavigateAround)
-            return newStateDueToAccident
+            return CarStateMachine.CarStateType.NAVIGATING_AROUND_ACCIDENT
         end
     end
-    --]====]
+
+    -- local newStateDueToAccident = CarStateMachine.handleYellowFlagZone(carIndex, car)
+    -- if newStateDueToAccident then
+        -- if CarManager.cars_navigatingAroundAccidentIndex[carIndex] ~= accidentIndex then
+            -- Logger.log(string.format("[CarState_NavigatingAroundAccident] Car %d switching to navigate around new accident #%d instead of previous accident #%d", carIndex, CarManager.cars_navigatingAroundAccidentIndex[carIndex], accidentIndex))
+            -- CarStateMachine.setStateExitReason(carIndex, StateExitReason.FoundCloserAccidentToNavigateAround)
+            -- return newStateDueToAccident
+        -- end
+    -- end
 
     -- if we are now past the car we are currently navigating around, and are some distance away from it, return to normal driving
     local carToNavigateAroundIndex = CarManager.cars_navigatingAroundCarIndex[carIndex]
