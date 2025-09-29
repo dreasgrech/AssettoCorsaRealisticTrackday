4904851d0020e3f978f1a374a997202eeb6937eb
Andreas Grech <agnt666@gmail.com>
2025-09-29 23:57:13 +0200

    comment

--- RaceFlagManager.lua
+++ RaceFlagManager.lua
@@ -1,6 +1,7 @@
 local RaceFlagManager = {}
 
 --- Sets the race flag to the specified type.
+--- Andreas: I think the flag only shows when the camera is focused on the local player car, not the AI cars
 ---@param flagType ac.FlagType
 RaceFlagManager.setRaceFlag = function(flagType)
         physics.overrideRacingFlag(flagType)
