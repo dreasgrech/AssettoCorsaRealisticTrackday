385e334ff842e479ac743f522b4b12413e7a16a0
Andreas Grech <agnt666@gmail.com>
2025-09-29 12:12:39 +0200

    refactoring

--- AccidentManager.lua
+++ AccidentManager.lua
@@ -10,24 +10,13 @@ AccidentManager.accidents_collidedWithTrack = {}
 AccidentManager.accidents_collidedWithCarIndex = {}
 AccidentManager.accidents_resolved = {}
 
--- TODO: when a car jumped (resets in ai flood or pits), we should also clear any accidents it was involved in
--- TODO: when a car jumped (resets in ai flood or pits), we should also clear any accidents it was involved in
--- TODO: when a car jumped (resets in ai flood or pits), we should also clear any accidents it was involved in
--- TODO: when a car jumped (resets in ai flood or pits), we should also clear any accidents it was involved in
-
-AccidentManager.informAboutCarReset = function(carIndex)
-    local accidentIndexAsCulprit = CarManager.cars_culpritInAccidentIndex[carIndex]
-    -- Logger.log(string.format("[AccidentManager] informAboutCarReset called for car #%d. culpritInAccident #%d.  Total accidents: %d", carIndex, accidentIndexAsCulprit, #AccidentManager.accidents_carIndex))
-    if accidentIndexAsCulprit > 0 then
-        -- Logger.log(string.format("AccidentManager: Car #%d has reset, clearing it from accident #%d", carIndex, accidentIndexAsCulprit))
-        Logger.log(string.format("[AccidentManager] Car #%d has reset, clearing accident #%d", carIndex, accidentIndexAsCulprit))
-
-        AccidentManager.accidents_carIndex[accidentIndexAsCulprit] = nil
-        AccidentManager.accidents_worldPosition[accidentIndexAsCulprit] = nil
-        AccidentManager.accidents_splinePosition[accidentIndexAsCulprit] = nil
-        AccidentManager.accidents_collidedWithTrack[accidentIndexAsCulprit] = nil
-        AccidentManager.accidents_collidedWithCarIndex[accidentIndexAsCulprit] = nil
-        AccidentManager.accidents_resolved[accidentIndexAsCulprit] = true
+local setAccidentAsResolved = function(accidentIndex)
+        AccidentManager.accidents_carIndex[accidentIndex] = nil
+        AccidentManager.accidents_worldPosition[accidentIndex] = nil
+        AccidentManager.accidents_splinePosition[accidentIndex] = nil
+        AccidentManager.accidents_collidedWithTrack[accidentIndex] = nil
+        AccidentManager.accidents_collidedWithCarIndex[accidentIndex] = nil
+        AccidentManager.accidents_resolved[accidentIndex] = true
 
         --CarManager.cars_culpritInAccidentIndex[carIndex] = nil
 
@@ -40,6 +29,16 @@ AccidentManager.informAboutCarReset = function(carIndex)
                 break
             end
         end
+end
+
+AccidentManager.informAboutCarReset = function(carIndex)
+    local accidentIndexAsCulprit = CarManager.cars_culpritInAccidentIndex[carIndex]
+    -- Logger.log(string.format("[AccidentManager] informAboutCarReset called for car #%d. culpritInAccident #%d.  Total accidents: %d", carIndex, accidentIndexAsCulprit, #AccidentManager.accidents_carIndex))
+    if accidentIndexAsCulprit > 0 then
+        -- Logger.log(string.format("AccidentManager: Car #%d has reset, clearing it from accident #%d", carIndex, accidentIndexAsCulprit))
+        Logger.log(string.format("[AccidentManager] Car #%d has reset, clearing accident #%d", carIndex, accidentIndexAsCulprit))
+
+        setAccidentAsResolved(accidentIndexAsCulprit)
     end
 end
 
@@ -130,16 +129,6 @@ AccidentManager.registerCollision = function(culpritCarIndex)
     CarManager.cars_culpritInAccidentIndex[culpritCarIndex] = accidentIndex
 
     Logger.log(string.format("Car #%02d COLLISION at (%.1f, %.1f, %.1f) with %s.  Total accidents: %d", culpritCarIndex, collisionLocalPosition.x, collisionLocalPosition.y, collisionLocalPosition.z, collidedWithTrack and "track" or ("car #" .. tostring(collidedWith)), lastAccidentIndexCreated, #AccidentManager.accidents_carIndex))
-
-    -- check the existing accidents and update the first non-resolved accident index so that loops iterating over accidents can start from there
-    for i = firstNonResolvedAccidentIndex, lastAccidentIndexCreated do
-        -- if not AccidentManager.accidents_resolved[i] then
-        if AccidentManager.accidents_resolved[i] == false then
-            -- Logger.log(string.format("AccidentManager: Updating firstNonResolvedAccidentIndex from %d to %d (last accident index: %d)", firstNonResolvedAccidentIndex, i, lastAccidentIndexCreated))
-            firstNonResolvedAccidentIndex = i
-            break
-        end
-    end
 
     return accidentIndex
 end
