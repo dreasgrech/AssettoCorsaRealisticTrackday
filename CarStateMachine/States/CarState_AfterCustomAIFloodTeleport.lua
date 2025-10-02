ce6ddd239966f45613d25cfa08558328ac094903
Andreas Grech <agnt666@gmail.com>
2025-10-02 15:40:31 +0200

    Failed experiment #2.1: The state of the cars when using the custom ai flood functionality => "AFTER_CUSTOMAIFLOOD_TELEPORT"
    Read the comment in CustomFloodManager.lua about why this state exists and also why we can't make use of it

---
+++ CarStateMachine/States/CarState_AfterCustomAIFloodTeleport.lua
@@ -0,0 +1,58 @@
+--[==========[
+local STATE = CarStateMachine.CarStateType.AFTER_CUSTOMAIFLOOD_TELEPORT
+
+--[===[
+Andreas: Read the comment in CustomFloodManager.lua about why this state exists and also why we can't make use of it
+--]===]
+
+CarStateMachine.CarStateTypeStrings[STATE] = "AfterCustomAIFloodTeleport"
+CarStateMachine.states_minimumTimeInState[STATE] = 0
+
+-- ENTRY FUNCTION
+CarStateMachine.states_entryFunctions[STATE] = function (carIndex, dt, sortedCarsList, sortedCarsListIndex, storage)
+      physics.setCarFuel(carIndex, 100)
+      physics.engageGear(carIndex, 3)
+      physics.setEngineRPM(carIndex, 5000)
+end
+
+-- UPDATE FUNCTION
+CarStateMachine.states_updateFunctions[STATE] = function (carIndex, dt, sortedCarsList, sortedCarsListIndex, storage)
+    ---@type ac.StateCar
+      local car = sortedCarsList[sortedCarsListIndex]
+      physics.setGentleStop(carIndex, false)
+      -- CarOperations.setPedalPosition(carIndex,CarOperations.CarPedals.Gas, 0.4)
+      physics.setAICaution(carIndex, 0)
+      -- physics.setCarVelocity(carIndex, car.look * 10)
+      -- physics.addForce
+      physics.awakeCar(carIndex)
+      physics.setAINoInput(carIndex, false, false)
+      -- physics.setCarFuel(carIndex, 100)
+      -- physics.engageGear(carIndex, 3)
+      -- physics.setEngineRPM(carIndex, 5000)
+end
+
+-- TRANSITION FUNCTION
+CarStateMachine.states_transitionFunctions[STATE] = function (carIndex, dt, sortedCarsList, sortedCarsListIndex, storage)
+      
+
+    ---@type ac.StateCar
+    -- local car = sortedCarsList[sortedCarsListIndex]
+    -- local carSpeed = car.speedKmh
+    -- if carSpeed > 60 then
+        -- return CarStateMachine.CarStateType.DRIVING_NORMALLY
+    -- end
+
+    local timeInCurrentState =CarManager.cars_timeInCurrentState[carIndex]
+    if timeInCurrentState > 20.0 then
+        return CarStateMachine.CarStateType.DRIVING_NORMALLY
+    end
+end
+
+-- EXIT FUNCTION
+CarStateMachine.states_exitFunctions[STATE] = function (carIndex, dt, sortedCarsList, sortedCarsListIndex, storage)
+      physics.setAICaution(carIndex, 1)
+      CarOperations.resetPedalPosition(carIndex, CarOperations.CarPedals.Gas)
+
+      CarManager.cars_justTeleportedDueToCustomAIFlood[carIndex] = false
+end
+--]==========]
