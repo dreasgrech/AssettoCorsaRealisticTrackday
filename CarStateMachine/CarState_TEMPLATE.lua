20d9bd0c212d76144bb92fef9ee63a292f6700dc
Andreas Grech <agnt666@gmail.com>
2025-10-02 20:28:15 +0200

    updating the state template with annotations

--- CarStateMachine/CarState_TEMPLATE.lua
+++ CarStateMachine/CarState_TEMPLATE.lua
@@ -9,21 +9,41 @@ CarStateMachine.CarStateTypeStrings[STATE] = "ThisState"
 CarStateMachine.states_minimumTimeInState[STATE] = 0
 
 -- ENTRY FUNCTION
+---@param carIndex integer
+---@param dt number
+---@param sortedCarsList table<integer,ac.StateCar>
+---@param sortedCarsListIndex integer
+---@param storage StorageTable
 CarStateMachine.states_entryFunctions[STATE] = function (carIndex, dt, sortedCarsList, sortedCarsListIndex, storage)
 
 end
 
--- UPDATE FUNCTION
+---UPDATE FUNCTION
+---@param carIndex integer
+---@param dt number
+---@param sortedCarsList table<integer,ac.StateCar>
+---@param sortedCarsListIndex integer
+---@param storage StorageTable
 CarStateMachine.states_updateFunctions[STATE] = function (carIndex, dt, sortedCarsList, sortedCarsListIndex, storage)
 
 end
 
 -- TRANSITION FUNCTION
+---@param carIndex integer
+---@param dt number
+---@param sortedCarsList table<integer,ac.StateCar>
+---@param sortedCarsListIndex integer
+---@param storage StorageTable
 CarStateMachine.states_transitionFunctions[STATE] = function (carIndex, dt, sortedCarsList, sortedCarsListIndex, storage)
 
 end
 
 -- EXIT FUNCTION
+---@param carIndex integer
+---@param dt number
+---@param sortedCarsList table<integer,ac.StateCar>
+---@param sortedCarsListIndex integer
+---@param storage StorageTable
 CarStateMachine.states_exitFunctions[STATE] = function (carIndex, dt, sortedCarsList, sortedCarsListIndex, storage)
 
 end
