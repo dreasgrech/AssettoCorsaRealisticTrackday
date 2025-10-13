111ac4f7de89d8aa919f28d955d21260fed61dc5
Andreas Grech <agnt666@gmail.com>
2025-10-13 22:22:11 +0200

    annotations

--- OnCarEventManager.lua
+++ OnCarEventManager.lua
@@ -14,11 +14,15 @@ OnCarEventManager.OnCarEventExecutions = { }
 local onCarEvents_eventTypeQueue = QueueManager.createQueue()
 local onCarEvents_carIndexQueue = QueueManager.createQueue()
 
+---Enqueues an on car event to be processed in the next tick
+---@param eventType OnCarEventManager.OnCarEventType
+---@param carIndex integer
 OnCarEventManager.enqueueOnCarEvent = function(eventType, carIndex)
     QueueManager.enqueue(onCarEvents_eventTypeQueue, eventType)
     QueueManager.enqueue(onCarEvents_carIndexQueue, carIndex)
 end
 
+---Should be called from an update loop to process any queued events
 OnCarEventManager.processQueuedEvents = function()
     while QueueManager.queueLength(onCarEvents_eventTypeQueue) > 0 do
         local eventType = QueueManager.dequeue(onCarEvents_eventTypeQueue)
