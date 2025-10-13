1a6257179d232a97b0229dca009c6a9e4c5231c2
Andreas Grech <agnt666@gmail.com>
2025-10-13 20:54:17 +0200

    moving the actual car events callbacks outside the manager

--- OnCarEventManager.lua
+++ OnCarEventManager.lua
@@ -1,54 +1,29 @@
 local OnCarEventManager = {}
 
 ---@enum OnCarEventManager.OnCarEventType 
-local OnCarEventType = {
+OnCarEventManager.OnCarEventType = {
     None = 0,
     Collision = 1,
     Jumped = 2,
 }
 
--- TODO: Get the actual implementation of these functions out of this file
-local onCarEventExecutions = {
-    ---The callback function for when a car collision event occurs
-    ---@param carIndex integer
-    [OnCarEventType.Collision] = function (carIndex)
-        local storage = StorageManager.getStorage()
-        if storage.handleAccidents then
-            -- Register an accident for the car collision
-            local accidentIndex = AccidentManager.registerCollision(carIndex)
-            if not accidentIndex then
-                return
-            end
-
-            CarStateMachine.informAboutAccident(accidentIndex)
-        end
-    end,
-    ---The callback function for when a car jumped event occurs
-    ---@param carIndex integer
-    [OnCarEventType.Jumped] = function (carIndex)
-      -- Inform the accident manager about the car reset
-      AccidentManager.informAboutCarReset(carIndex)
-
-      -- finally reset all our car data
-      if not CarManager.cars_justTeleportedDueToCustomAIFlood[carIndex] then
-        CarManager.setInitializedDefaults(carIndex)
-      end
-    end,
-}
+--This is filled with actual implementations from somewhere else
+---@type table<OnCarEventManager.OnCarEventType,function>
+OnCarEventManager.OnCarEventExecutions = { }
 
 local onCarEvents_eventTypeQueue = QueueManager.createQueue()
 local onCarEvents_carIndexQueue = QueueManager.createQueue()
 
-OnCarEventManager.OnCarEventType = OnCarEventType
 OnCarEventManager.enqueueOnCarEvent = function(eventType, carIndex)
     QueueManager.enqueue(onCarEvents_eventTypeQueue, eventType)
     QueueManager.enqueue(onCarEvents_carIndexQueue, carIndex)
 end
+
 OnCarEventManager.processQueuedEvents = function()
     while QueueManager.queueLength(onCarEvents_eventTypeQueue) > 0 do
         local eventType = QueueManager.dequeue(onCarEvents_eventTypeQueue)
         local carIndex = QueueManager.dequeue(onCarEvents_carIndexQueue)
-        onCarEventExecutions[eventType](carIndex)
+        OnCarEventManager.OnCarEventExecutions[eventType](carIndex)
     end
 end
 
