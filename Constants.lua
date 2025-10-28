1fdbe0a8679abf62f2ed1f9c20985e278992e000
Andreas Grech <agnt666@gmail.com>
2025-10-28 01:36:03 +0100

    adding the app version

--- Constants.lua
+++ Constants.lua
@@ -3,6 +3,7 @@ local Constants = {}
 local sim = ac.getSim()
 
 Constants.APP_NAME = "RealisticTrackday"
+Constants.APP_VERSION = "0.9.5"
 Constants.APP_ICON_PATH = 'AssettoCorsaRealisticTrackday_Icon.png'
 Constants.APP_ICON_SIZE = vec2(128, 128)
 Constants.IS_ONLINE = sim.isOnlineRace
