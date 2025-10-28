df3209f6cf0ec8e7f48eca01d88b93688d8ea1d5
Andreas Grech <agnt666@gmail.com>
2025-10-28 18:43:34 +0100

    adding the timestamp to the logs

--- Logger.lua
+++ Logger.lua
@@ -2,22 +2,33 @@ local Logger = {}
 
 -- CSP log file at 'Documents\Assetto Corsa\logs\custom_shaders_patch.log'
 
+local APP_NAME_STRING = string.format("[%s]", Constants.APP_NAME)
+
+local getTimestampString = function()
+  -- return os.date("%Y-%m-%d %H:%M:%S")
+  return string.format("[%s]", os.date("%H:%M:%S"))
+end
+
+local getFullMessage = function(msg)
+  return string.format("%s %s %s", getTimestampString(), APP_NAME_STRING, msg)
+end
+
 ---Logs the message to the CSP log
 ---@param msg string
 function Logger.log(msg)
-  ac.log('[' .. Constants.APP_NAME .. '] ' .. msg)
+  ac.log(getFullMessage(msg))
 end
 
 ---Logs the message as a warning to the CSP log
 ---@param msg string
 function Logger.warn(msg)
-  ac.warn('[' .. Constants.APP_NAME .. '] ' .. msg)
+  ac.warn(getFullMessage(msg))
 end
 
 ---Logs the message as an error to the CSP log
 ---@param msg string
 function Logger.error(msg)
-  ac.error('[' .. Constants.APP_NAME .. '] ' .. msg)
+  ac.error(getFullMessage(msg))
 end
 
 return Logger
